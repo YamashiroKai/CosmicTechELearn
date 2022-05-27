0e4c8ef94000d4d6b2a773a985bfadbd37cbaeed
Tris <62794221+YamashiroKai@users.noreply.github.com>
2022-05-27 22:28:48 +0200

    Add files via upload

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using ELearn.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELearn.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Item> Items { get; set; }// Saves the data from our database and entity inside<> and table name 
+        //public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ Models/Fighters.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ELearn.Models
+{
+    public class Fighters
+    {
+        [Key]
+        public int FighterID { set; get; }
+
+        public string BenderName { set; get; }
+
+        public string City { set; get; }
+
+        public string Weapon { set; get; }
+
+        public string[] FightingStyles { set; get; }
+    }
+}
