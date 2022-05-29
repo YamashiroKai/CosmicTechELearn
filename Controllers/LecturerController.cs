a94ce9083eeb524e5e405a514dd9ff0aeb5a0f61
Tris <62794221+YamashiroKai@users.noreply.github.com>
2022-05-29 18:45:53 +0200

    Add files via upload

---
+++ Controllers/LecturerController.cs
@@ -0,0 +1,32 @@
+﻿using ELearn.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELearn.Controllers
+{
+    public class LecturerController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Submission()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
