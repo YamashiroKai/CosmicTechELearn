4ccf5c3355bf36cc0384eed01bf65d0af4db4caa
Tris <62794221+YamashiroKai@users.noreply.github.com>
2022-08-12 02:52:23 +0200

    Material, Course and Module

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ELearn.Models;
+
+namespace ELearn.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
